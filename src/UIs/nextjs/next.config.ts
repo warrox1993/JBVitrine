d321a0b102fbac4cc0587a6f726229e9207d6880
Jean-Baptiste <jeanbaptiste.dhondt1@gmail.com>
2025-11-25 11:18:55 +0100

    fix(nextjs): allow innowise.com images in next.config.ts

--- src/UIs/nextjs/next.config.ts
+++ src/UIs/nextjs/next.config.ts
@@ -72,6 +72,14 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "innowise.com",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
   },
 
   turbopack: {
